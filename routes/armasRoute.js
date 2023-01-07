3c3b9efc14a6caadcf3158273b905baf45e531dd
Geraldo Vicente <geraldovicente987@gmail.com>
2023-01-07 16:17:20 -0300

    Pequenas alterações

---
+++ routes/armasRoute.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+
+// const armasController = require('../controllers/armasController');
+
+// const router = express.Router();
+
+// router
+//     .route('/')
+//     .get(armasController.obterTodasArmas)
+//     .post(armasController.criarArma);
+
+// router
+//     .route('/:id')
+//     .get(armasController.obterUmaArma)
+//     .patch(armasController.atualizarArma)
+//     .delete(armasController.deletarArma);
+
+// module.exports = router;
---
+++ routes/valorantRoute.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+
+// const valorantController = require('../controllers/valorantController');
+
+// const router = express.Router();
+
+// router.route('/').get(valorantController.obterTudo);
+
+// module.exports = router;
