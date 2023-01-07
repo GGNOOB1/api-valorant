89dcc456f7f9d247d6d29cf12baa1e5da93e475a
Geraldo Vicente <geraldovicente987@gmail.com>
2023-01-07 12:46:38 -0300

    Criado arquivo para conter as informações dos manipuladores de rotas para as rotas de armas | Só a parte básica

---
+++ controllers/armasController.js
@@ -0,0 +1,27 @@
+// const fs = require('fs');
+
+// //const armasValorant = JSON.parse(fs.readFileSync('./armas.json', 'utf-8'));
+
+// exports.obterTodasArmas = (req, res) => {
+//     res.status(200).json({
+//         status: 'Sucess',
+//         data: {
+//             campeoes: campeoesValorant,
+//         },
+//     });
+// };
+
+// exports.obterUmaArma = (req, res) => {
+//     res.status(200).json({
+//         status: 'Sucess',
+//         data: {
+//             campeoes: campeoesValorant,
+//         },
+//     });
+// };
+
+// exports.criarArma = (req, res) => {};
+
+// exports.atualizarArma = (req, res) => {};
+
+// exports.deletarArma = (req, res) => {};
