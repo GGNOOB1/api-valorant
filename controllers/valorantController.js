119f7204af98c3d784145c3147045d3d617eb4d8
Geraldo Vicente <geraldovicente987@gmail.com>
2023-01-07 12:48:22 -0300

    Criado arquivo de funções manipuladoras de rotas para a rota que mostrará todos os dados do jogo Valorant

---
+++ controllers/valorantController.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+//const armasValorant = JSON.parse(fs.readFileSync('./armas.json', 'utf-8'));
+
+// exports.obterTudo = (req, res) => {
+//     res.status(200).json({
+//         status: 'Sucess',
+//         data: {
+//             campeoes: campeoesValorant,
+//         },
+//     });
+// };
