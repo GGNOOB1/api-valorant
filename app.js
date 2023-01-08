cb1fa119604539c1b47cd6fff45eb271963d69b1
Geraldo Vicente <geraldovicente987@gmail.com>
2023-01-08 17:35:10 -0300

    Descomentando as rotas valorant e armas

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const valorantRoute = require('./routes/valorantRoute');
 const app = express();
 app.use(express.json());
 
-// app.use('/api/v1/agentes', valorantRoute);
+app.use('/api/v1/valorant', valorantRoute);
 app.use('/api/v1/agentes', campeoesRoute);
-// app.use('/api/v1/armas', armasRoute);
+app.use('/api/v1/armas', armasRoute);
 
 module.exports = app;
