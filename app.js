8d1803b66de63fffec562a4b8e886a11663d2fca
Geraldo Vicente <geraldovicente987@gmail.com>
2023-01-08 22:19:09 -0300

    Renomeando nomes de importações e comentando

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 
+// Importando rotas de armas,
 const armasRoute = require('./routes/armasRoute');
-const campeoesRoute = require('./routes/agentesRoute');
+const agentesRoute = require('./routes/agentesRoute');
 const valorantRoute = require('./routes/valorantRoute');
 
+// Criando app express
 const app = express();
+
+// O app usa a middleware funcion que analisa solicitações recebidas com cargas JSON
 app.use(express.json());
 
+// Funções middlewares que serão executadas quando o usuário fazer uma
+//solicitação http para as rotas especificadas
 app.use('/api/v1/valorant', valorantRoute);
-app.use('/api/v1/agentes', campeoesRoute);
+app.use('/api/v1/agentes', agentesRoute);
 app.use('/api/v1/armas', armasRoute);
 
+// Exportando app para o server.js, onde será gerenciado o servidor e banco de dados
 module.exports = app;
